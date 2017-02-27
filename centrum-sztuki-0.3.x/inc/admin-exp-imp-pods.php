289126d4a2ceb4a39c4204e2b6a728f668ebcb6a
jurek2006 <jurek2006@gmail.com>
2017-02-27 11:42:20 +0100

    Eksperymenty z eksportowaniem - importowaniem filmów w pods

--- centrum-sztuki-0.3.x/inc/admin-exp-imp-pods.php
+++ centrum-sztuki-0.3.x/inc/admin-exp-imp-pods.php
@@ -1,1 +1,26 @@
 <h1>Export - import PODS</h1>
+
+<?php 
+	$params = array(
+        'limit' => -1
+    );
+    $pods = pods( 'filmy2' , $params );
+    $all_data = $pods->export_data();
+    if ( !empty( $all_data ) ) {
+        // die(json_encode($all_data));
+        print_r($all_data);
+
+        foreach ($all_data as $key => $value) {
+        	echo "<h2>\$all_data[$key]</h2>";
+        	print_r($value);
+
+	        $podsIMP = pods('filmy');
+	        $ids = $podsIMP->import($value);
+	        printf("Dodano: ".$ids);
+        }
+    }
+    else {
+    	printf("Nie ma filmów");
+        // die(json_encode(array('error' => 'No filmy found.')));
+    }
+?>
