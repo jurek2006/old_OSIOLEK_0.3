77334466765fe519133a1b0fcaa0be8d5b4fbff8
jurek2006 <jurek2006@gmail.com>
2017-03-01 13:24:27 +0100

    Nowy sposób eksportu filmów
    Z ustawianą (na razie na sztywno) daty filtra ostatniej modyfikacji
    filmu. Z pozbyciem się eksportowania pola 'projekcje' (które bardzo
    "śmieci" przy każdym filmie).

--- centrum-sztuki-0.3.x/inc/admin-export-pods.php
+++ centrum-sztuki-0.3.x/inc/admin-export-pods.php
@@ -6,40 +6,46 @@
 
     if(isset( $_POST["exp_filmy"])){
     //Jeśli kliknięto przycisk "Eksportuj filmy"
+    //Pobierane są wszystkie filmy (z pods PODS_FILMY), których ostatnia modyfikacja nastąpiła najpóźniej w dniu określonym datą $datetime
+    //Są one encodowane do JSON, a ścieżka home_url jest zamieniana na string określony w HOME_URL_ALIAS
+    //Na końcu ten JSON wyświetlany jest w textarea
+
+        // ustawienia daty - filtru które filmy mają być wyeksportowane (brana pod uwagę jest ostatnia modyfikacja)
+        $datetime = new DateTime('2017-02-01');
+        $termin_poczatek = $datetime->format('Y-m-d');
 
         $params = array(
-            'limit' => -1
+            'limit' => -1,
+            'where' => 'DATE( post_modified ) >= "'.$termin_poczatek.'"'
         );
         $pods = pods( PODS_FILMY , $params );
-        $all_data = $pods->export_data();
-        if ( empty( $all_data ) ) {
-        // jeśli nie ma  żadnych znalezionych danych w pods, a więc $all_data jest empty
-            exit('Nie znaleziono żadnych filmów');
-        }
-        else {
-        // jeśli znaleziono filmy i są w tablicy $all_data
-            foreach ($all_data as $key => $value) {
-                printf($key.': ');
-                // print_r($value);
-                $value['projekcje'] = '';
-                print_r($value['post_title']);
-                print_r($value['projekcje']);
-                echo '<br><br>';
-            }
+
+        $all_data = array(); //tablica na zapisanie wszystkich eksportowanych danych
+
+        if ( $pods->total() > 0 ) {
+
+            while ( $pods->fetch() ) {
+                $tytul_filmu =  $pods->display('post_title');
+                $id = $pods->field('id');
+                $single_data = $pods->export();
+                $single_data['projekcje'] = ''; //wyczyszczenie pola 'projekcje', które nie jest w żaden sposób potrzebne, a zaśmieca dużą ilością danych
+
+                $all_data[$id] = $single_data;
+                
+            }//while ( $pods->fetch() )
 
             $all_data['rodzaj_danych'] = 'filmy'; //dodanie pola na rodzaj danych, żeby przy imporcie można było sprawdzić czy to są te dane, które powinny być
-
             $all_data_json = json_encode($all_data);
-            
+            $home_url_json = trim (json_encode(home_url()), '"' ); //zakodowanie home_url w json (z pozbyciem się średników na początku i końcu) - na potrzeby wyszukiwania i podmiany ścieżek
+
+            $all_data_json = str_replace($home_url_json, HOME_URL_ALIAS, $all_data_json); //zamiana ścieżek zawierających home_url na string zdefiniowany w HOME_URL_ALIAS
+
+            echo sprintf('<textarea name="textarea" rows="10"  style="width: 100%%">%s</textarea>', $all_data_json); //wstawienie danych zakodowanych w json do textare
+
+        }//$pods = pods( PODS_FILMY , $params )
+        else{
+            echo 'Nie znaleziono żadnych filmów spełniających kryterium eksportu.'
         }
-
-        $home_url_json = trim (json_encode(home_url()), '"' ); //zakodowanie home_url w json (z pozbyciem się średników na początku i końcu) - na potrzeby wyszukiwania i podmiany ścieżek
-
-        $all_data_json = str_replace($home_url_json, HOME_URL_ALIAS, $all_data_json); //zamiana ścieżek zawierających home_url na string zdefiniowany w HOME_URL_ALIAS
-
-        echo sprintf('<textarea name="textarea" rows="10"  style="width: 100%%">%s</textarea>', $all_data_json); //wstawienie danych zakodowanych w json do textarea
-
-        
 
     }//if(isset( $_POST["exp_filmy"]))
     else{
