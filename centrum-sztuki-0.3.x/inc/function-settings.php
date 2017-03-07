115d07fea60aaa880a6ab62e7598b2e5aafb4510
jurek2006 <jurek2006@gmail.com>
2017-03-07 12:33:09 +0100

    Usunięcie wszystkich elementów z eksperymentów importu i eksportu

--- centrum-sztuki-0.3.x/inc/function-settings.php
+++ centrum-sztuki-0.3.x/inc/function-settings.php
@@ -20,16 +20,6 @@
 	// ADRES SYSTEMU BILETOWEGO VISUALTICKET DLA CSO
 	define("ADRES_VISUALTICKET", "https://s7.systembiletowy.pl/cso/");
 
-	// USTAWIENIA EKSPORTU i IMPORTU danych PODS (używane w inc/admin-import-pods.php i inc/admin-export-pods.php)
-	// ------
-
-	// BLOKOWANIE OPCJI IMPORTU PODS (w inc/admin-import-pods.php) na wersji produkcyjnej (czyli na www.kultura.olawa.pl)
-	define("BLOKUJ_IMPORT_PODS", true);
-
-	// alias home_url przy eksportowaniu danych do tekstu (json) i później importowaniu
-	define("HOME_URL_ALIAS", "{home_url}");
-
-	define("PODS_FILMY","filmy"); //nazwa pods służącego do zapisywania filmów (używana na razie tylko przy import/export)
 	// ------------------------------------------------------------------------------------------------------------------------
 
 	// UPRAWNIENIA
@@ -47,10 +37,4 @@
 
 	// uprawnienia potrzebne do wyświetlania w dashboard menu zarządzania ekranem repertuaru w kasie
 	define("UPR_DSH_MENU_KASA_EKRAN", 'edit_ekran_kasa_biletowa');
-
-	// uprawnienia potrzebne do wyświetlania w dashboard strony export pods (i menu export - import pods)
-	define("UPR_DSH_MENU_EXPORT_PODS", 'export');
-
-	// uprawnienia potrzebne do wyświetlania w dashboard strony import pods 
-	define("UPR_DSH_MENU_IMPORT_PODS", 'import');
 ?>
